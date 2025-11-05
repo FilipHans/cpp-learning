fc927fd27a1cfdab04778ae68a139c67131a901a
FilipHans <filip.hansen98@gmail.com>
2025-11-05 15:51:42 +0100

    first commit

---
+++ hello-world-1/helloworld.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+
+// 
+
+/*
+
+
+*/
+
+int main(){
+
+    // std::cout << "I like pizzsdfsdfsdfa!" << std::endl;
+    // std::cout << "c++ is uphill" << std::endl;
+
+    int x;
+    x = 5;
+    int y = 6;
+    int sum = x + y;
+
+
+    int age = 21;
+    
+    // integer is only whole numbers
+
+    // double number including decimal
+
+    double price = 10.99;
+
+    char grade = 'A';
+    char initial = 'B';
+
+
+    bool student;
+    student = true;
+    
+    student = false;
+
+    std::string name = "Filip";
+    
+
+    std::cout << price << '\n';
+    std::cout << x << '\n';
+    std::cout << sum << '\n';
+    return 0;
+
+}
